74438382346f9b9c2b65f587c7dc6310714803b5
Rob <r0b.dev@outlook.com>
2017-12-30 11:54:43 +0100

    Added layout test example (snapshot)

---
+++ tests/layout.test.tsx
@@ -0,0 +1,11 @@
+import {shallow} from "enzyme";
+import * as React from "react";
+import Layout from "../components/layout";
+
+describe("Layout component", () => {
+    it("should render correctly", () => {
+        const wrapper = shallow(<Layout title={""} description={""}/>);
+
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+});
